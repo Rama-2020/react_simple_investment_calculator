ed7d25e6850517f9704fcb27bf0cbf7ea12ea716
Rama-2020 <ramiroscerra@hotmail.com>
2024-05-20 12:06:08 +0200

    Added Styles to the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import logo from '../assets/logo_coin.png';
 
 export default function Header(){
-    return <header>
+    return <header id='header'>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Investment Calculator</h1>
     </header>
